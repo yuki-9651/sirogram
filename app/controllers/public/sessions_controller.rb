b398311aa5afbab3d67ef36cca4bc909d7829205
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-08-20 04:34:46 +0000

    [ADD]Bootstrapを使ったレイアウト整備

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  layout 'admin'
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   # before_action :configure_sign_in_params, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  
   protected
 
   
-  def after_sign_in_path_for(resource)
-    admin_dashboards_path # ログイン後にリダイレクトするパス
-  end
-
-  def after_sign_out_path_for(resource_or_scope)
-    new_admin_session_path # ログアウト後にリダイレクトするパス
-  end
-
-
-  protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
   # GET /resource/sign_in
   # def new
   #   super
