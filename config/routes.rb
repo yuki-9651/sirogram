c74d2412d979494dded0ad8e5f9336f41ac18a2e
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-09-19 09:38:59 +0000

    [ADD]アプリケーション反映

--- config/routes.rb
+++ config/routes.rb
@@ -23,14 +23,15 @@ Rails.application.routes.draw do
     get 'homes/about' => 'homes#about', as: 'about'
     get 'homes/top' => 'homes#top'
     get 'searches', to: 'searches#search', as: 'search' 
-    get  '/users/check' => 'users#check'
-    patch  '/users/withdraw' => 'users#withdraw'
+    get '/users/check', to: 'users#check', as: 'users_check'
+    patch '/users/withdraw', to: 'users#withdraw', as: 'withdraw_user'
+
     resources :groups, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
       resource :permits, only: [:create, :destroy] 
       resource :group_memberships, only: [:create, :destroy]
       get "join" => "groups#join"
     end
-    get "groups/:id/permits" => "groups#permits", as: :permits
+    get "groups/:id/permits", to: "groups#permits", as: :permits
     
     resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
       resources :comments, only: [:create, :destroy]
@@ -39,13 +40,12 @@ Rails.application.routes.draw do
         get 'tag_search', to: 'searches#search'
       end
     end
+
     get '/users/:name', to: 'users#show', as: 'user_show_by_name'
     get '/users/:user_name/edit', to: 'users#edit', as: 'edit_user_by_name'
    
     resources :users, only: [:show, :edit, :update, :destroy] do
       resources :posts, only: [:index]
     end
-    
   end
-
 end
